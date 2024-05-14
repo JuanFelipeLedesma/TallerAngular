fafd9e8730b0b33468f9797fcf1d0e29db40c6fd
Juan Ledesma <j.ledesma@uniandes.edu.co>
2024-05-14 18:44:29 -0500

    minor fix

--- src/app/series/listar-series/listar-series.component.ts
+++ src/app/series/listar-series/listar-series.component.ts
@@ -23,6 +23,7 @@ export class ListarSeriesComponent implements OnInit {
     this.http.get<any[]>('https://gist.githubusercontent.com/josejbocanegra/8490b48961a69dcd2bfd8a360256d0db/raw/34ff30dbc32392a69eb0e08453a3fc975a3890f0/series.json')
       .subscribe(data => {
         this.series = data;
+        this.calculateAverageSeasons();
       });
   }
   calculateAverageSeasons(): void {
